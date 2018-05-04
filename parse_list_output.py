eb6063d38c8bc33ddfeb4b8c7603bcea44b5a7a5
Kurt Strecker <ksphotopro@gmail.com>
2018-05-04 00:44:14 -0400

    initial parse subdirs

--- parse_list_output.py
+++ parse_list_output.py
@@ -7,16 +7,39 @@ with open('directory.txt', 'r', encoding='utf-8') as raw_file:
 
 primary_categories = []
 
+secondary_categories = []
+
+dirs = []
+
 for line in lines:
     if line[-1:] == ":" and line[0] != "/":
         primary_categories.append(line)
 
 # print(primary_categories[0])
 
-ref_index = 9
+ref_index = 2
 
-for line in lines:
-    if line[-1:] == ":":
+for line in enumerate(lines):
+    if line[1][-1:] == ":":
         # print(line[1:len(primary_categories[0])])
-        if line[1:len(primary_categories[ref_index])] == primary_categories[ref_index][:-1]:
-            print(line)
+        if line[1][1:len(primary_categories[ref_index])] == primary_categories[ref_index][:-1]:
+            secondary_categories.append(line)
+
+i = 0
+
+for dir in secondary_categories:
+    
+    while i < len(secondary_categories) - 1:
+        span = secondary_categories[i + 1][0] - secondary_categories[i][0]
+        # print(secondary_categories[i + 1][0], secondary_categories[i][0])
+        temp_dirs = lines[secondary_categories[i][0]:secondary_categories[i + 1][0]]
+        for line in temp_dirs:
+            if line[-1:] != ":" and line[-1:] != "\n":
+                print(secondary_categories[i][1][:-1] + "/" + line)
+        dirs.append(temp_dirs)
+
+        i += 1
+
+# for line in dirs:
+#     for entry in line:
+#         print(entry)
