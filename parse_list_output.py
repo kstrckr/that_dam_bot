c02bdefae2abdb9968948509a12f4d79b89c2230
Kurt Strecker <ksphotopro@gmail.com>
2018-05-04 00:54:02 -0400

    19777 paths generated

--- parse_list_output.py
+++ parse_list_output.py
@@ -17,13 +17,14 @@ for line in lines:
 
 # print(primary_categories[0])
 
-ref_index = 2
+ref_index = range(0,10)
 
-for line in enumerate(lines):
-    if line[1][-1:] == ":":
-        # print(line[1:len(primary_categories[0])])
-        if line[1][1:len(primary_categories[ref_index])] == primary_categories[ref_index][:-1]:
-            secondary_categories.append(line)
+for i in ref_index:
+    for line in enumerate(lines):
+        if line[1][-1:] == ":":
+            # print(line[1:len(primary_categories[0])])
+            if line[1][1:len(primary_categories[i])] == primary_categories[i][:-1]:
+                secondary_categories.append(line)
 
 i = 0
 
@@ -35,11 +36,12 @@ for dir in secondary_categories:
         temp_dirs = lines[secondary_categories[i][0]:secondary_categories[i + 1][0]]
         for line in temp_dirs:
             if line[-1:] != ":" and len(line) > 0:
-                print(secondary_categories[i][1][:-1] + "/" + line)
-        dirs.append(temp_dirs)
+                path = secondary_categories[i][1][:-1] + "/" + line
+                dirs.append(path)
 
         i += 1
 
+print(len(dirs), dirs[0])
 # for line in dirs:
 #     for entry in line:
 #         print(entry)
