e3242acdde725da083b6b78759329a8f640c5e4b
Gilt Groupe Studios <GGS@iMac-8.local>
2018-05-24 15:23:39 -0400

    modulized import files

---
+++ bot_modules/__init__.py
@@ -0,0 +1,5 @@
+from bot_modules.authenticator import Authenticator
+from bot_modules.database_setup_and_seed import DbSetup, DbInterface
+from bot_modules.parse_stills_txt_to_path import StillDamDirs
+from bot_modules.zoom_checkout import ZmCheckoutSession
+from bot_modules.zoom_ls_to_text import ZmLsToText
